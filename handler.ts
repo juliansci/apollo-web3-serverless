e5abeaf4b63b80b5eb2e8092b3403d7acf2095a7
Julian Scialabba <julian.scialabba@gmail.com>
2021-11-01 03:02:25 -0300

    Add CORS and remove web3-eth-contract

--- handler.ts
+++ handler.ts
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import { ApolloServer } from 'apollo-server-lambda';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,24 +1,23 @@
-const Web3 = require('web3');
-const Contract = require('web3-eth-contract');
-const ERC20Abi = require('../contracts/abi/ERC20.json');
-
+import Web3 from 'web3';
+import ERC20Abi from '../contracts/abi/ERC20.json';
+import { AbiItem } from 'web3-utils';
 const PROVIDER_URL = process.env.WEB3_PROVIDER_URL;
 
+const web3 = new Web3(PROVIDER_URL || '');
+
 export const getEthCurrentBlock = async () => {
-  const web3 = new Web3(PROVIDER_URL);
   return await web3.eth.getBlockNumber();
 };
 
-export const getERC20TotalSupply = async (address: String) => {
-  Contract.setProvider(PROVIDER_URL);
-  const tokenContract = new Contract(ERC20Abi, address);
+export const getERC20TotalSupply = async (address: string) => {
+  const tokenContract = new web3.eth.Contract(ERC20Abi as AbiItem[], address);
   return await tokenContract.methods.totalSupply().call({});
 };
 
 const resolvers = {
   Query: {
     getEthCurrentBlock,
-    getERC20TotalSupply: (_: any, { address }: { address: String }) => getERC20TotalSupply(address.toString()),
+    getERC20TotalSupply: (_: any, { address }: { address: string }): Promise<string> => getERC20TotalSupply(address.toString()),
   },
 };
 
